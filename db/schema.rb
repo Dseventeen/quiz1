a8a804d74995d4285e6dffc9c06aa55e97582ca1
Dan <dseventeen@gmail.com>
2019-09-18 18:27:50 +0000

    addd wines in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_122933) do
+ActiveRecord::Schema.define(version: 2019_09_18_180541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
