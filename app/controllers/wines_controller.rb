8f0c8a904734890f2cbe38553d07700c3c6e839d
Dan <dseventeen@gmail.com>
2019-11-01 02:08:18 +0000

    finished quiz

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,6 +1,21 @@
 class WinesController < ApplicationController
 
-def index
-  @wines = Wine.all
-end
+  def index
+    @wine = Wine.all
+  end
+    
+  def new
+    @wine = Wine.new
+  end
+  def create
+    Wine.create(wine_params)
+    redirect_to root_path
+  end
+
+
+private
+
+  def wine_params
+    params.require(:wine).permit(:name, :year, :producer)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'wines#index'
+  resources :wines
 end
